951aedf2f0b81b9aeb6ecf20bb30849ca8a9cc44
pageshih <a0981863141@gmail.com>
2021-08-23 23:33:43 +0800

    8/23 gulp problem

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,11 +1,11 @@
-import Swiper from '../../../node_modules/swiper/swiper-bundle';
+
 
 
 
 var swiper = new Swiper(".mySwiper", {
   watchSlidesProgress: true,
   watchSlidesVisibility: true,
-  slidesPerView: 4.5,
+  slidesPerView: 2.8,
 });
 
 $(function() {
